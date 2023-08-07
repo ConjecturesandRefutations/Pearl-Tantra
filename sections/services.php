fe3bad98b8e37893ee3b8950eeb530a25bcc785f
Alfie Collins <alfiecollins44@gmail.com>
2023-08-07 11:36:42 +0100

    adding the 000webhost changes

--- sections/services.php
+++ sections/services.php
@@ -1,35 +1,80 @@
-<section class="container section scrollspy" id="package">
-  <div class="row">
-    <div class="col s12">
-      <h2>All inclusive Package...</h2>
-      
-      <p>
-      Mind & Overview
-<br/><br/>
-Art of Touch (receiving is giving)
-<br/><br/>
-Transmission
-<br/><br/>
-Meditation & Mantra & Ritual
-<br/><br/>
-Sacred Relationship (intimacy & sexuality ~ polarity ~ relational expansion deepening)
-<br/><br/>
- Shadow Keys & Gifts
-<br/><br/>
-Archetypes 
-<br/><br/>
-Tantrik Mapping (Genetic Coding intro ~ additional option) 
-<br/><br/>
-The ongoing integration of this practice is based upon contemplation, the sweet spot to be discovered between concentration (effort) and meditation (no effort).
-<br/><br/>
-Those drawn to Pearl Tantra are willing to be guided through intuitive processes and are open to the flow of the feminine way. Having a clearly defined container allows relaxation into the true receptivity, sensitivity, beauty, chaos, creativity, nourishment, vulnerability and strength of The Deep Feminine Itself, which in turn gives rise to The Divine Masculine, within the individual and therefore greater collective. Pearl Tantra is birthed of my experience. In putting all questions, ideas and expectations aside, the offering is for pure receiving through direct experience.
-<br/><br/>
-You are invited to trust, take a pause with me, and enter the stream into the wildness of your ecstatic heart.
-<br/><br/>
-And so, just as many pathways that lead here continue from here as One.
-<br/><br/>
-“To be devoted means to lie forever in the lap of the Divine.” – Richard Rudd
-</p>
+<section class="container section scrollspy" id="what">
+    
+    <div class="row">
+      <div class="col s12 l4">
+        <img src="img/pearl.jpg" alt="portrait" class="responsive-img materialboxed img">
+      </div>
+      <div class="col s12 l6 offset-l1">
+        <h2>Practice</h2>
+        <p>Emanating as Pearl Tantra, a non-dual shamanic tantrik yoga practice born in Cape Town. Tantra loosely refers to the transmutation of sexual or dense frequency energy into Divine Energy.
+          Our direct reference point is the body, where much is held, revealed, released and integrated. The bulk and initial groundwork is made up of embodiment practices – 
+          breathwork, massage, touch and movement drawing from a range of deeply lived methods like yoga asanas and pranayama, voicing, fascia release, trauma releasing exercises…
+        </p>
+      </div>
     </div>
-  </div>
-</section>
+    <div class="row">
+      <div class="col s12 l4 push-l7 offset-l1">
+        <img src="img/lynyoga2.jpg" alt="portrait" class="responsive-img materialboxed img">
+      </div>
+      <div class="col s12 l6 pull-l5 right-align offset-l1">
+        <p>Sessions typically consist of the powerful combo of consult inquiry and embodiment. The practice is for individuals as well as couples or small groups. 
+          <br><br>
+         Online sessions are a worthwhile exploration in and of themselves and now is the perfect time. As with all things working online has some limitations, however most parts of the practice can be included and it is proving a valuable container for creativity, mirroring and consolidating connection with oneself. I am enlivened by the opportunity for new emerging creations and in establishing solid foundations toward potential practise in person.</p>
+          <p>For sincere practitioners who are called and ready, I facilitate next level energetic activation and initiation.
+          </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s12 l4">
+        <img src="img/lynyoga3.jpg" alt="portrait" class="responsive-img materialboxed img" id="sand">
+      </div>
+      <div class="col s12 l6 offset-l1">
+          <h6>Insight into the roots of our exploration… </h6>
+          <h7>What Is Who Is?? </h7>
+        <p>An embodied journey toward surrendered Presence, through which life force can flow and we are truly breathed moved and expressed 
+          perhaps even into orgasmicness for Life itself.</p>
+          <h7>Heart Light Sound</h7>
+          <p>Heart activation and healing takes place through working with light and sound energy frequencies (sound is light in the form 
+            of motion, a way to move light). Meditating on the receiving and giving loop of sustainable energy, to a final resting into the abyss of silence and the stillness of nothingness.</p>
+                      <h7>Ceremony</h7>
+            <p>In slowing down, we bring consciousness and reverence into as many moments as we can. In this opening to Life there is the gift of the channel for communion and prayer, through 
+              which we can also integrate Oneness down into daily life as ceremony. Ordinary takes on new depth of meaning and sparkle of essence.</p>
+      </div>
+    </div>
+    
+    <div class="row">
+        <div class="col s12 m6">
+            <h4>All-inclusive package…</h4>
+            <p>Transmission </p>
+            <p>Living Meditation </p>
+            <p>Inquiry</p>
+            <p>Mind & Overview</p>
+            <p>Art of Touch (receiving is giving)</p>
+            <p>Transformational Process of Grief</p>
+            <p>Shadow Keys & Gifts</p>
+            <p>Archetypes</p>
+            <p>Sacred Relationship (intimacy & sexuality ~ polarity ~ relational expansion deepening)</p>
+            <p>Tantrik Mapping (Genetic Coding intro ~ additional option)</p>
+        </div>
+        <div class="col s12 m6">
+          <img src='./img/vertebra.jpg' class="responsive-img materialboxed">
+          </div>
+    </div>
+    
+        <div class="row">
+             <div class="col s12 m6">
+                 <p>The ongoing integration of this practice is based upon contemplation, the sweet spot to be discovered between concentration (effort) and meditation (no effort). 
+                     Those drawn to Pearl Tantra are willing to be guided through intuitive processes and are open to the flow of the feminine way. Having a clearly defined container allows relaxation into the true receptivity, sensitivity, beauty, chaos, creativity, nourishment, vulnerability and strength of The Deep Feminine Itself, which in turn gives rise to The Divine Masculine, within the individual and therefore greater collective. Pearl Tantra is birthed of my experience. 
+                </p>
+             </div>
+             <div class="col s12 m6"> 
+                <p>In putting all questions, ideas and expectations aside, the offering is for pure receiving through direct experience. </p>
+                <p>You are invited to trust, take a pause with me, and enter the stream into the wildness of your ecstatic heart. </p>
+                <p>And so, just as many pathways that lead here continue from here as One. </p>
+                <p>“To be devoted means to lie forever in the lap of the Divine.” – Richard Rudd </p>
+                <p>Mana XXX </p>
+
+            </div>    
+        </div>
+
+  </section>
--- sections/what.php
+++ sections/what.php
@@ -2,54 +2,48 @@
     
     <p class='container mt-4 black-text'>Many pathways lead here – from simply being curious about Tantra to the seeking of intimacy to that longing existential inquiry – and all is welcomed with humility and grace. 
 
- 
+ <br/><br/>
 
 So what whispers set your heart on fire?!?!?! </p>
     
 </section>
 
-<section class="container section scrollspy" id="what">
+<section class="container section scrollspy " id="package">
+ 
+      <h2>About</h2>
+      
+<div class="row">
+      
+      <div class="col s12 m4">
+      <img src='./img/profile.jpg' class="responsive-img materialboxed">
+      </div>
+      
+      <div class="col s12 m8"> <p>Open to the free energy of The Muse, propelled by the heart and flowing with all of life's myriad flavours - aesthetics, relating, music, abundance, nature, sustainability... </p>
+
+<p>Trained as a photographer and Montessori teacher. A devoted Tantra practitioner with a solid Yoga foundation. All this refracted through the lens of a profound underworld journey of addiction and a subsequent 15 year recovery process has gifted me with a refined sensitivity, insight and compassion to support others in the integration of shadow light. </p>
+
+<p>***Initiations with fellow teachers and practitioners (to be written)... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+</div>
+</div>
+
+<div class="row">
+
+<div class="col s12 m4">
     
-    <div class="row">
-      <div class="col s12 l4">
-        <img src="img/pearl.jpg" alt="portrait" class="responsive-img materialboxed">
-      </div>
-      <div class="col s12 l6 offset-l1">
-        <h2>What is it?</h2>
-        <p>Emanating as Pearl Tantra, a non-dual shamanic tantrik yoga practice born in Cape Town. Tantra loosely refers to the transmutation of sexual or dense frequency energy into Divine Energy.
-          Our direct reference point is the body, where much is held, revealed, released and integrated. The bulk and initial groundwork is made up of embodiment practices – 
-          breathwork, massage, touch and movement drawing from a range of deeply lived methods like yoga asanas and pranayama, voicing, fascia release, trauma releasing exercises…
-        </p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col s12 l4 push-l7 offset-l1">
-        <img src="img/lynyoga2.jpg" alt="portrait" class="responsive-img materialboxed">
-      </div>
-      <div class="col s12 l6 pull-l5 right-align offset-l1">
-        <p>Sessions typically consist of the powerful combo of consult inquiry and embodiment. Initially guided by myself – 
-          tantrika and founder of Pearl Tantra, Mana Muse – the practice is for individuals as well as couples.
-          <br><br>
-         In-person sessions are dependent on my location and discretion.</p>
-         <p>Online sessions are a worthwhile exploration in and of themselves and now is the perfect time. 
-          As with all things working online has some limitations, however most parts of the practice can be 
-          included and it is proving a valuable container for creativity, mirroring and consolidating connection with oneself. 
-          I am enlivened by the opportunity for new emerging creations and in establishing solid foundations toward potential practise in person.</p>
-          <p>For sincere practitioners who are called and ready, I facilitate next level energetic activation and initiation.
-          </p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col s12 l4">
-        <img src="img/lynyoga3.jpg" alt="portrait" class="responsive-img materialboxed">
-      </div>
-      <div class="col s12 l6 offset-l1">
-        <p>An embodied journey toward surrendered Presence, through which life force can flow and we are truly breathed moved and expressed 
-          perhaps even into orgasmicness for Life itself.</p>
-          <p>Heart activation and healing takes place through working with light and sound energy frequencies (sound is light in the form 
-            of motion, a way to move light). Meditating on the receiving and giving loop of sustainable energy, to a final resting into the abyss of silence and the stillness of nothingness.</p>
-            <p>In slowing down, we bring consciousness and reverence into as many moments as we can. In this opening to Life there is the gift of the channel for communion and prayer, through 
-              which we can also integrate Oneness down into daily life as ceremony. Ordinary takes on new depth of meaning and sparkle of essence.</p>
-      </div>
-    </div>
-  </section>
+<p>An honouring pace and cyclical way between stillness and wild spirited movement is supported by a well developed emotional intelligence and therefore ability to embrace uncertainty and surrender to the moment. </p>
+
+<p>I live to embody Love and naturally inspire deep engagement and creativity with Life to many. </p>
+
+<p>Founder and Creative Director of Pearl Tantra. </p>
+
+</div>
+
+<div class="col s12 m8 img">
+<img src='./img/sand.jpg' class="responsive-img materialboxed">
+</div>
+
+
+</div>
+
+</section>
